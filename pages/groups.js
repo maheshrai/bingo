0c3499c722055f70cf7ea5be6ccf0a49bfe4ed18
Mahesh Rai <mahesh.rai@aps.com>
2021-01-02 07:50:01 -0700

    Added functionality to add members to group

--- pages/groups.js
+++ pages/groups.js
@@ -41,7 +41,13 @@ import {
 function Groups() {
   let [session, setSession] = useState(null);
   const [groupName, setGroupName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [groups, setGroups] = useState([]);
+
+  const handleGroupNameChange = (event) => setGroupName(event.target.value);
+  const handleNameChange = (event) => setName(event.target.value);
+  const handleEmailChange = (event) => setEmail(event.target.value);
 
   const toast = useToast();
 
@@ -58,13 +64,58 @@ function Groups() {
     let email = supabase.auth.session().user.email;
     let { data: group, error } = await supabase
       .from("group")
-      .select(`id,name,owner,groupmember(group)`)
+      .select(`id,name,owner,groupmember(id, name)`)
       .eq("owner", email);
     console.log(JSON.stringify(group));
     setGroups(group);
   }
 
-  const handleInputChange = (event) => setGroupName(event.target.value);
+  async function deleteGroup(id) {
+    let deleted = await supabase.from("groupmember").delete().eq("group", id);
+    deleted = await supabase.from("group").delete().eq("id", id);
+    fetchMyGroups();
+  }
+
+  async function addGroupMember(group) {
+    const { data, error } = await supabase
+      .from("groupmember")
+      .insert([{ name: name, email: email, group: group }]);
+    fetchMyGroups();
+  }
+
+  async function createGroup() {
+    const { data, error } = await supabase
+      .from("group")
+      .insert([{ name: groupName, owner: session.user.email }]);
+    if (error) {
+      console.log("Error logging out:", error.message);
+      toast({
+        title: "failed to create group",
+        description: error.message,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+    } else {
+      // Add the owner as group member
+      const { d, e } = await supabase.from("groupmember").insert([
+        {
+          group: data[0].id,
+          name: session.user.email,
+          email: session.user.email,
+        },
+      ]);
+      console.log(JSON.stringify(data));
+      toast({
+        title: "Group created",
+        description: "Group was created. " + data[0].id,
+        status: "success",
+        duration: 1000,
+        isClosable: true,
+      });
+      fetchMyGroups();
+    }
+  }
 
   return (
     <Box p={5} shadow="md" borderWidth="1px" flex="1" borderRadius="md">
@@ -86,7 +137,7 @@ function Groups() {
                   <FormLabel>Name</FormLabel>
                   <Input
                     type="text"
-                    onChange={handleInputChange}
+                    onChange={handleGroupNameChange}
                     value={groupName}
                   />
                   <FormHelperText>Name of your new bingo group</FormHelperText>
@@ -95,32 +146,7 @@ function Groups() {
                   <Spacer></Spacer>
                   <Button
                     colorScheme="green"
-                    onClick={async () => {
-                      const { data, error } = await supabase
-                        .from("group")
-                        .insert([
-                          { name: groupName, owner: session.user.email },
-                        ]);
-                      if (error) {
-                        console.log("Error logging out:", error.message);
-                        toast({
-                          title: "failed to create group",
-                          description: error.message,
-                          status: "error",
-                          duration: 4000,
-                          isClosable: true,
-                        });
-                      } else {
-                        console.log(JSON.stringify(data));
-                        toast({
-                          title: "Groiup created",
-                          description: "Group was created. " + data[0].id,
-                          status: "success",
-                          duration: 1000,
-                          isClosable: true,
-                        });
-                      }
-                    }}
+                    onClick={async () => createGroup()}
                   >
                     Create
                   </Button>
@@ -145,11 +171,14 @@ function Groups() {
                     {g.name}
                   </Heading>
                   <Spacer></Spacer>
-                  <IconButton
-                    aria-label="Delete group"
-                    color="tomato"
-                    icon={<DeleteIcon />}
-                  />
+                  {session.user.email === g.owner && (
+                    <IconButton
+                      aria-label="Delete group"
+                      color="tomato"
+                      onClick={() => deleteGroup(g.id)}
+                      icon={<DeleteIcon />}
+                    />
+                  )}
                 </HStack>
                 <Divider></Divider>
                 <HStack padding="10px">
@@ -159,15 +188,13 @@ function Groups() {
                   <WrapItem>
                     <Box p="6">
                       <Wrap>
-                        {groups &&
-                          groups.groupmember &&
-                          groups.groupmember.map((g) => (
-                            <WrapItem>
-                              <Tooltip label="Jimmie Hayes" fontSize="md">
-                                <Avatar name="mahesh.rai@gmail.com" />
-                              </Tooltip>
-                            </WrapItem>
-                          ))}
+                        {g.groupmember.map((gm) => (
+                          <WrapItem>
+                            <Tooltip label={gm.name} fontSize="md">
+                              <Avatar name={gm.name} />
+                            </Tooltip>
+                          </WrapItem>
+                        ))}
                       </Wrap>
                     </Box>
                   </WrapItem>
@@ -175,13 +202,50 @@ function Groups() {
                 <HStack>
                   <Spacer></Spacer>
 
-                  <IconButton
-                    aria-label="Edit"
-                    icon={<PlusSquareIcon />}
-                    color="blue.500"
-                  />
-
-                  <Link href="/bingo/1">
+                  <Popover>
+                    <PopoverTrigger>
+                      <IconButton
+                        aria-label="Edit"
+                        icon={<PlusSquareIcon />}
+                        color="blue.500"
+                      />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                      <PopoverArrow />
+                      <PopoverCloseButton />
+                      <PopoverHeader>Add member</PopoverHeader>
+                      <PopoverBody>
+                        <FormControl id="name">
+                          <FormLabel>Name</FormLabel>
+                          <Input
+                            type="text"
+                            onChange={handleNameChange}
+                            value={name}
+                          />
+                          <FormHelperText>Name of the member</FormHelperText>
+                        </FormControl>
+                        <FormControl id="email">
+                          <FormLabel>Email</FormLabel>
+                          <Input
+                            type="text"
+                            onChange={handleEmailChange}
+                            value={email}
+                          />
+                          <FormHelperText>Email of the member</FormHelperText>
+                        </FormControl>
+                        <HStack>
+                          <Spacer></Spacer>
+                          <Button
+                            colorScheme="green"
+                            onClick={async () => addGroupMember(g.id)}
+                          >
+                            Add
+                          </Button>
+                        </HStack>
+                      </PopoverBody>
+                    </PopoverContent>
+                  </Popover>
+                  <Link href={"/bingo/" + g.id}>
                     <Button color="green.500">Play</Button>
                   </Link>
                 </HStack>
