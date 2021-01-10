ea444ecc6179a1a6bcc78db17b7e08253daebaec
Mahesh Rai <mahesh.rai@aps.com>
2021-01-10 16:20:31 -0700

    Updated footer

--- pages/_app.js
+++ pages/_app.js
@@ -45,14 +45,7 @@ function MyApp({ Component, pageProps }) {
         <Header></Header>
         <Component {...pageProps} />
         <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
+          Copyright &copy; 2021 Ishaan Inc.
         </footer>
       </ChakraProvider>
     </UserContext.Provider>
